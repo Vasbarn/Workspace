33200ceb022a6c2891a2cf3fa4970595fe4da3d4
vasilievas <vasilievbarnaul@mail.ru>
2023-11-28 18:01:02 +0700

    Делаю автобекап

--- Main folder/Ручные бекапы.py
+++ Main folder/Ручные бекапы.py
@@ -4,11 +4,21 @@ import os
 class HDDir:
     def __init__(self):
         self.__path_on_hd_main = "D:" + os.sep + "Резервные копии"
-        self.__dir_with_path = {
+        self.__dir_hd_with_path = {
             "Бухгалтерия": os.path.join(self.__path_on_hd_main, "Бухгалтерия"),
             "ЗУП": os.path.join(self.__path_on_hd_main, "ЗУП"),
             "Охрана труда": os.path.join(self.__path_on_hd_main, "Охрана труда"),
             "Торговля": os.path.join(self.__path_on_hd_main, "Торговля"),
+        }
+
+        self.__path_on_70_main = 2 * os.sep + os.path.join("192.168.8.70", "Backup")
+        self.__path_on_62_main = 2 * os.sep + os.path.join("192.168.8.62","e$","Data", "Backup")
+        self.__dir_back__with_path = {
+            "Бухгалтерия": os.path.join(self.__path_on_70_main, "accounting3"),
+            "Бухгалтерия 2": os.path.join(self.__path_on_70_main, "accounting_black"),
+            "ЗУП": os.path.join(self.__path_on_70_main, "salary3"),
+            "Охрана труда": os.path.join(self.__path_on_70_main, "ot-soft"),
+            "Торговля": self.__path_on_62_main
         }
 
         self.__prefix_accounting = "accounting3_backup_"
@@ -18,9 +28,9 @@ class HDDir:
         self.__prefix_trade = "trade_backup_"
 
     def get_all_wanted_files(self) -> dict:
-        """Собираем все файлы на флешке в папках которые есть"""
+        """Собираем все файлы на флешке в папках которые есть. Для последующего удаления"""
         result = dict()
-        for key, value in self.__dir_with_path.items():
+        for key, value in self.__dir_hd_with_path.items():
             if key == "Бухгалтерия":
                 files = os.listdir(value)
                 data = filter(lambda name_file: name_file.startswith(self.__prefix_accounting), files)
@@ -54,9 +64,35 @@ class HDDir:
                 for path_file in temp.values():
                     os.remove(path_file)
 
+    def start_copy(self):
+        """Возвращает список нужны файлов в директории где бекапы"""
+        result = dict()
+        for key, value in self.__dir_back__with_path.items():
+            if key == "Бухгалтерия":
+                files = os.listdir(value)
+                data = filter(lambda name_file: name_file.startswith(self.__prefix_accounting), files)
+                result[key] = list(map(lambda file_name: os.path.join(value, file_name), data))
+            elif key == "Бухгалтерия 2":
+                files = os.listdir(value)
+                data = filter(lambda name_file: name_file.startswith(self.__prefix_accounting_black), files)
+                result[key] = list(map(lambda file_name: os.path.join(value, file_name), data))
+            elif key == "ЗУП":
+                files = os.listdir(value)
+                data = filter(lambda name_file: name_file.startswith(self.__prefix_salary3), files)
+                result[key] = list(map(lambda file_name: os.path.join(value, file_name), data))
+            elif key == "Охрана труда":
+                files = os.listdir(value)
+                data = filter(lambda name_file: name_file.startswith(self.__prefix_ot_soft), files)
+                result[key] = list(map(lambda file_name: os.path.join(value, file_name), data))
+            elif key == "Торговля":
+                files = os.listdir(value)
+                data = filter(lambda name_file: name_file.startswith(self.__prefix_trade), files)
+                result[key] = list(map(lambda file_name: os.path.join(value, file_name), data))
+        return result
 
 if __name__ == "__main__":
     proc = HDDir()
-    proc.del_old()
+    # proc.del_old()
+    proc.start_copy()
 
 
