7ace3202b8c57c25ebe5413d047bdb7a8a67684a
vasilievas <vasilievbarnaul@mail.ru>
2024-02-27 19:22:20 +0700

    Наконец-то осилил списки

--- Main folder/Получение список битрикс.py
+++ Main folder/Получение список битрикс.py
@@ -6,22 +6,32 @@ key = "fdrwucecw1uvpel1"
 url = f"https://bitrix.tg-alterra.ru/rest/1166/{key}/{method}"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 data = {
-    'IBLOCK_TYPE_ID': 'lists_socnet',
-    'IBLOCK_CODE': 'list178',
-    # 'filter': {
-    #     "ACTIVE": True
-    # },
+    'IBLOCK_TYPE_ID': 'bitrix_processes',
+    'IBLOCK_ID': '182',
 }
 result = []
 with requests.Session() as session:
     response: dict = dict(session.post(url=url, data=json.dumps(data), headers=headers).json())
-    result.extend(response.get("result"))
-    print(response.get("next"))
-    while response.get("next"):
-        data['start'] = response.get("next")
-        response: dict = dict(session.post(url=url, data=json.dumps(data), headers=headers).json())
-        print(response.get("next"))
-        result.extend(response.get("result"))
-        break
-for elem in result:
-    print(elem)
+    print(response)
+    data = {
+        'IBLOCK_TYPE_ID': 'bitrix_processes',
+        'IBLOCK_ID': '182',
+    }
+    method = "lists.get"
+    url = f"https://bitrix.tg-alterra.ru/rest/1166/{key}/{method}"
+    response: dict = dict(session.post(url=url, data=json.dumps(data), headers=headers).json())
+    print(response)
+    method = "lists.field.get"
+    url = f"https://bitrix.tg-alterra.ru/rest/1166/{key}/{method}"
+    response: dict = dict(session.post(url=url, data=json.dumps(data), headers=headers).json())
+    print(response)
+#     result.extend(response.get("result"))
+#     print(response)
+#     while response.get("next"):
+#         data['start'] = response.get("next")
+#         response: dict = dict(session.post(url=url, data=json.dumps(data), headers=headers).json())
+#         print(response.get("next"))
+#         result.extend(response.get("result"))
+#         break
+# for elem in result:
+#     print(elem)
